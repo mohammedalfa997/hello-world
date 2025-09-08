ea19350b9646e03c5e2aec68666b0e375aaf4cd6
mohammedalfa997 <alfamohammed00239@gmail.com>
2025-09-08 14:12:50 +0100

    my first code

---
+++ hello-world.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+print(4 + 4)
+
+print(8 - 3)
+
+print(3 * 7)
+
+print(8 / 2)
+
+box1 = 2
+box2 = 1
+print(box1 + box2)
+
+b1 = 2
+b2 = 3
+
+c1 = 4
+c2 = 5
+print(b1 + b2 + c1 + c2)
