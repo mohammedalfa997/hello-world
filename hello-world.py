0ee8ff830b673f0989d43f8026a9737b453e4228
mohammedalfa997 <alfamohammed00239@gmail.com>
2025-09-10 13:56:25 +0100

    i leant concatenation and how to put two or more word together

--- hello-world.py
+++ hello-world.py
@@ -18,3 +18,40 @@ b2 = 3
 c1 = 4
 c2 = 5
 print(b1 + b2 + c1 + c2)
+
+print (3)
+
+firstName = "Ashafa"
+lastname = "Mohammed"
+fullName = firstName + " " + lastname + "."
+print(fullName)
+
+product = "tecno"
+model = "smart8"
+productfullname = product + model
+print(productfullname)
+fullname = product + " " + model
+
+x1 = "h"
+x2 = "e"
+x3 = "l"
+x4 = "l"
+x5 = "o"
+
+word = x1 + x2 + x3 + x4 + x5
+print(word)
+y1 = "w"
+y2 = "o"
+y3 = "r"
+y4 = "ld"
+word1 = y1 + y2 + y3 + y4
+print(word1)
+print(word + " " + word1)
+
+print(word)
+firstcampany = "dadinkowa"
+lastcampany = "brande"
+fullcampany = firstcampany + lastcampany 
+print(fullcampany)
+fullcampany = firstcampany + " " + lastcampany + "."
+print(fullcampany)
